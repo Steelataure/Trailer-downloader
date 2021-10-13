f15185bd896290c877ce6990eeb620f0deda72eb
Steel <59340020+Steelataure@users.noreply.github.com>
2021-10-13 21:37:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,59 +18,59 @@ path_trailer = 'trailer/'
 downloads_path = (str(Path.home() / "Downloads").replace("\\", "/") + '/')
 
 listeFichiers = []
-
-
-def files_management():
-    for (repertoire, sousRepertoires, fichiers) in walk(path_film):
-        listeFichiers.extend(fichiers)
-        print(listeFichiers)
-
-        break
-
-
-# ----------------------------------------------------
 is_on = True
-
-
-def download():
-    for n in range(len(listeFichiers)):
-        if not (not ('.mp4' in listeFichiers[n]) and not ('.avi' in listeFichiers[n]) and not (
-                '.mkv' in listeFichiers[n]) and not ('.mov' in listeFichiers[n]) and not (
-                '.webm' in listeFichiers[n]) and not ('.mpeg' in listeFichiers[n])):
-
-            selection = str(listeFichiers[n][0:20])
-            s = Search(selection + " Bande d'annonce VF")
-            new = str((s.results[0]))
-
-            with open('assets/stats.txt', 'r+') as files:
-                files.write(new + '\n')
-
-            with open('assets/stats.txt', 'r+') as files:
-                ouverture = (files.readline(52).split('Id='))
-
-                id = (ouverture[+1])
-
-            video = f"https://www.youtube.com/watch?v={id}"
-
-            if is_on:
-                webbrowser.open(f"https://www.youtube.com/watch?v={id}")
-
-            yt = YouTube(video)
-            yt.streams \
-                .filter(progressive=True, file_extension='mp4') \
-                .order_by('resolution') \
-                .desc() \
-                .first() \
-                .download('trailer/')
-
-
-# ------------------------------------------------
 
 
 class Application(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.creer_widgets()
+
+
+    def files_management(self):
+        test = self.path_entry.get()
+        for (repertoire, sousRepertoires, fichiers) in walk(test):
+            listeFichiers.extend(fichiers)
+            print(listeFichiers)
+            print(self.path_entry.get())
+
+            break
+        else:
+            print("Le chemin n'existe pas ou n'est pas bien indiqué")
+            # Mettre une alerte box
+
+
+    def download(self):
+        for n in range(len(listeFichiers)):
+            if not (not ('.mp4' in listeFichiers[n]) and not ('.avi' in listeFichiers[n]) and not (
+                    '.mkv' in listeFichiers[n]) and not ('.mov' in listeFichiers[n]) and not (
+                    '.webm' in listeFichiers[n]) and not ('.mpeg' in listeFichiers[n])):
+
+                selection = str(listeFichiers[n][0:15])  # Nombre de caractères pris
+                s = Search(selection + " Bande d'annonce VF")
+                new = str((s.results[0]))
+
+                with open('assets/stats.txt', 'r+') as files:
+                    files.write(new + '\n')
+
+                with open('assets/stats.txt', 'r+') as files:
+                    ouverture = (files.readline(52).split('Id='))
+
+                    id = (ouverture[+1])
+
+                video = f"https://www.youtube.com/watch?v={id}"
+
+                if is_on:
+                    webbrowser.open(f"https://www.youtube.com/watch?v={id}")
+
+                yt = YouTube(video)
+                yt.streams \
+                    .filter(progressive=True, file_extension='mp4') \
+                    .order_by('resolution') \
+                    .desc() \
+                    .first() \
+                    .download(self.path_entry2.get())
+
 
     def creer_widgets(self):
         background_image = PhotoImage(file="assets/Trailer2.png")
@@ -107,7 +107,8 @@ class Application(tk.Tk):
 
         # Button Start
         self.Start_image = PhotoImage(file='assets/Button.png')
-        self.Button_start = tk.Button(self, command=start, image=self.Start_image, borderwidth=0, bg='WHITE',
+        self.Button_start = tk.Button(self, command=self.start,
+                                      image=self.Start_image, borderwidth=0, bg='WHITE',
                                       activebackground='WHITE')
         self.Button_start.place(x=210, y=450)
 
@@ -126,9 +127,10 @@ class Application(tk.Tk):
             is_on = True
 
 
-def start():
-    files_management()
-    download()
+    def start(self):
+        self.files_management()
+        self.download()
+
 
 
 def main():
@@ -137,6 +139,7 @@ def main():
     app.geometry('720x630')
     app.resizable(False, False)
     app.iconbitmap('assets/trailer.ico')
+
     app.mainloop()
 
 
